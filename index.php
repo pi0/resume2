cbd80704b1ca90d723bd3d64c3af16b44245489b
sabaak95 <saba.ak95@gmail.com>
2017-10-05 15:19:56 +0330

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+
+<head>
+
+<title>FlexyCodes</title>
+
+	<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+
+	<meta name="description" content="FlexyCodes"/>
+
+</head>
+
+<body>
+	
+	<img src="http://flexy-codes.com/images/under-construction.jpg" style="width:100%" />
+</body>
+</html>
